eb6f6f52da728bfdd4c955e9a98006f02e601045
abgros <58993004+abgros@users.noreply.github.com>
2022-04-15 11:41:31 -0400

    Update board.py

--- board.py
+++ board.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw, ImageFont
+import PIL.Image, PIL.ImageDraw, PIL.ImageFont
 from re import findall
 
 DARK = (181, 136, 99)
@@ -16,166 +16,191 @@ FONT_LOCATION = r'C:\Windows\Fonts\ARLRDBD.ttf'
 
 
 class DrawBoard:
-    def __init__(self, fen, folder='chess', lastmove=None, upside_down=False, board_type=None, flip_pieces=False):
-        self.fen = fen
-        self.folder = folder
-        self.lastmove = lastmove
-        self.upside_down = upside_down
-        self.board_type = board_type
-        self.flip_pieces = flip_pieces
-    
-    def flatten(self, some_list):
-        output = []
-        for item in some_list:
-            if isinstance(item, list): 
-                output += self.flatten(item)
-            else: 
-                output += [item]
-        return output
+	def __init__(self, board_type, folder, flip_pieces, upside_down, fen, lastmove):
+		self.fen = fen
+		self.folder = folder
+		self.lastmove = lastmove
+		self.upside_down = upside_down
+		self.board_type = board_type
+		self.flip_pieces = flip_pieces
+	
+	def flatten(self, some_list):
+		output = []
+		for item in some_list:
+			if isinstance(item, list):
+				output += self.flatten(item)
+			else:
+				output += [item]
+		return output
 
-    def square_to_coords(self, square, width, height):
-        if self.upside_down:
-            x = width - ALPHABET.index(square[0]) - 1
-            y = int(square[1:]) - 1
-        else:
-            x = ALPHABET.index(square[0])
-            y = height - int(square[1:]) 
-        return [x*SQ_SIZE, y*SQ_SIZE]
+	def square_to_coords(self, square, width, height):
+		if self.upside_down:
+			x = width - ALPHABET.index(square[0]) - 1
+			y = int(square[1:]) - 1
+		else:
+			x = ALPHABET.index(square[0])
+			y = height - int(square[1:])
+		return [x*SQ_SIZE, y*SQ_SIZE]
 
-    def get_piece_img(self, piece):
-        if piece.lower() == piece:
-            colour = "b"
-        else:
-            colour = "w"
-            
-        try:
-            return Image.open(f'assets\\{self.folder}\\{colour}{piece}.png').convert('RGBA')
-        except:
-            return Image.open(f'assets\\fail.png').convert('RGBA')
+	def get_piece_img(self, piece):
+		if piece.lower() == piece:
+			colour = "b"
+		else:
+			colour = "w"
 
-    def fen_to_array(self):
-        fen_array = [findall('(\d+|\+?[a-zA-Z]\~?)', i) for i in self.fen.split(' ')[0].split('[')[0].split('/')]
+		try:
+			return PIL.Image.open(f'assets\\{self.folder}\\{colour}{piece}.png').convert('RGBA')
+		except:
+			return PIL.Image.open(f'assets\\fail.png').convert('RGBA')
 
-        for i in range(len(fen_array)):
-            rank = [[""]*int(j) if j.isnumeric() else j for j in fen_array[i]]
-            fen_array[i] = self.flatten(rank)
-            
-        if self.upside_down:
-            fen_array = [i[::-1] for i in fen_array][::-1]
-            
-        return fen_array
+	def fen_to_array(self):
+		fen_array = [findall('(\d+|\+?[a-zA-Z]\~?)', i) for i in self.fen.split(' ')[0].split('[')[0].split('/')]
 
-    def in_hand(self):
-        try:
-            pocket = self.fen.split('[')[1].split(']')[0]
-        except:
-            return ([], [])
-        blackpcs = sorted([p for p in pocket if p.islower()])
-        whitepcs = sorted([p for p in pocket if p.isupper()])
-        return (whitepcs, blackpcs)
+		for i in range(len(fen_array)):
+			rank = [[""]*int(j) if j.isnumeric() else j for j in fen_array[i]]
+			fen_array[i] = self.flatten(rank)
 
-    def render_board(self, img_name):
-        pos = self.fen_to_array()
-        b_height = len(pos)
-        b_width = len(pos[0])
+		if self.upside_down:
+			fen_array = [i[::-1] for i in fen_array][::-1]
 
-        white_hand, black_hand = self.in_hand()
-        pocket_rows = 2 + (len(white_hand)-1) // b_height + (len(black_hand)-1) // b_height
+		return fen_array
 
-        spacer = 0
-        if pocket_rows:
-            spacer = round(SQ_SIZE*0.2)
-        
-        img_size = (b_width*SQ_SIZE, (b_height + pocket_rows)*SQ_SIZE + spacer)
+	def in_hand(self):
+		try:
+			pocket = self.fen.split('[')[1].split(']')[0]
+		except:
+			return ([], [])
+		blackpcs = sorted([p for p in pocket if p.islower()])
+		whitepcs = sorted([p for p in pocket if p.isupper()])
+		return (whitepcs, blackpcs)
 
-        img = Image.new(mode='RGB', size=img_size, color=GREY)
-        drw = ImageDraw.Draw(img, 'RGBA')
+	def draw_board(self, stabilise_pocket=False):
+		pos = self.fen_to_array()
+		b_height = len(pos)
+		b_width = len(pos[0])
 
-        # BOARD
-        if self.board_type == "checkerboard": # chess style checkerboard
-            for i in range(b_height):
-                for j in range(b_width):
-                    if (i+j) % 2 == 0:
-                        drw.rectangle([j*SQ_SIZE, i*SQ_SIZE, (j+1)*SQ_SIZE, (i+1)*SQ_SIZE], fill=LIGHT)
-                    else:
-                        drw.rectangle([j*SQ_SIZE, i*SQ_SIZE, (j+1)*SQ_SIZE, (i+1)*SQ_SIZE], fill=DARK)
+		white_hand, black_hand = self.in_hand()
+		pocket_rows = 2 + (len(white_hand)-1) // b_height + (len(black_hand)-1) // b_height
 
-        elif isinstance(self.board_type, list): # checkerboard with custom colours
-            for i in range(b_height):
-                for j in range(b_width):
-                    if (i+j) % 2 == 0:
-                        drw.rectangle([j*SQ_SIZE, i*SQ_SIZE, (j+1)*SQ_SIZE, (i+1)*SQ_SIZE], fill=self.board_type[0])
-                    else:
-                        drw.rectangle([j*SQ_SIZE, i*SQ_SIZE, (j+1)*SQ_SIZE, (i+1)*SQ_SIZE], fill=self.board_type[1])
-            
-        elif isinstance(self.board_type, tuple): # shogi style board with a custom colour
-            border = round(SQ_SIZE*0.01)
-            drw.rectangle([0, 0, b_width*SQ_SIZE, b_height*SQ_SIZE], fill=self.board_type)
-            for i in range(b_height-1):
-                drw.rectangle([0, SQ_SIZE*(i+1) - border, b_width*SQ_SIZE, SQ_SIZE*(i+1) + border], fill=BLACK)            
-            for i in range(b_height-1):
-                drw.rectangle([SQ_SIZE*(i+1) - border, 0, SQ_SIZE*(i+1) + border, b_height*SQ_SIZE], fill=BLACK)                        
+		# To make the game clip more stable, try to keep the pocket size at 2
+		if stabilise_pocket and pocket_rows == 1:
+			pocket_rows = 2
 
-        else: # custom image
-            board = Image.open(f'assets\\{self.folder}\\board.png')
-            if self.upside_down:
-                board = board.rotate(180)
-            img.paste(board, (0, 0))
-        
-        # LETTERS
-        font_size = SQ_SIZE*0.2
-        font = ImageFont.truetype(FONT_LOCATION, round(font_size))
+		spacer = 0
+		if pocket_rows:
+			spacer = round(SQ_SIZE*0.2)
 
-        ranks = [str(n+1) for n in range(b_height)]
-        if not self.upside_down:
-            ranks = ranks[::-1]
+		img_size = (b_width*SQ_SIZE, (b_height + pocket_rows)*SQ_SIZE + spacer)
 
-        files = [ALPHABET[n] for n in range(b_width)]
-        if self.upside_down:
-            files = files[::-1]
-        
-        for i in range(b_height):
-            drw.text((b_width*SQ_SIZE - round(font_size*0.1), i*SQ_SIZE + round(font_size*0.2)), ranks[i], fill=(0, 0, 0), font=font, anchor="rt")
+		img = PIL.Image.new(mode='RGB', size=img_size, color=GREY)
+		drw = PIL.ImageDraw.Draw(img, 'RGBA')
 
-        for i in range(b_width):
-            drw.text((i*SQ_SIZE + round(font_size*0.1), b_height*SQ_SIZE - round(font_size*0.1)), files[i], fill=(0, 0, 0), font=font, anchor="ls")
+		# BOARD
+		if self.board_type == "checkerboard": # chess style checkerboard
+			for i in range(b_height):
+				for j in range(b_width):
+					if (i+j) % 2 == 0:
+						drw.rectangle([j*SQ_SIZE, i*SQ_SIZE, (j+1)*SQ_SIZE, (i+1)*SQ_SIZE], fill=LIGHT)
+					else:
+						drw.rectangle([j*SQ_SIZE, i*SQ_SIZE, (j+1)*SQ_SIZE, (i+1)*SQ_SIZE], fill=DARK)
 
-        # HIGHLIGHTING
-        if self.lastmove:
-            for highlight in findall(r'[a-z]\d+', self.lastmove):
-                coords = self.square_to_coords(highlight, b_width, b_height)
-                highlight_coords = coords + [i+100 for i in coords]
-                if self.board_type == "checkerboard":
-                    if sum(coords) % (SQ_SIZE*2) == 0:
-                        drw.rectangle(highlight_coords, fill=LASTMOVE_LIGHT)
-                    else:
-                        drw.rectangle(highlight_coords, fill=LASTMOVE_DARK)
-                else:
-                    drw.rectangle(highlight_coords, fill=GREEN)
+		elif isinstance(self.board_type, list): # checkerboard with custom colours
+			for i in range(b_height):
+				for j in range(b_width):
+					if (i+j) % 2 == 0:
+						drw.rectangle([j*SQ_SIZE, i*SQ_SIZE, (j+1)*SQ_SIZE, (i+1)*SQ_SIZE], fill=self.board_type[0])
+					else:
+						drw.rectangle([j*SQ_SIZE, i*SQ_SIZE, (j+1)*SQ_SIZE, (i+1)*SQ_SIZE], fill=self.board_type[1])
 
-        # PIECES
-        for i in range(len(pos)):
-            for j in range(len(pos[i])):
-                if pos[i][j]:
-                    piece = self.get_piece_img(pos[i][j])
-                    if self.flip_pieces:
-                        piece = piece.rotate(180)
-                    img.paste(piece, (j*SQ_SIZE, i*SQ_SIZE), piece)
+		elif isinstance(self.board_type, tuple): # shogi style board with a custom colour
+			border = round(SQ_SIZE*0.01)
+			drw.rectangle([0, 0, b_width*SQ_SIZE, b_height*SQ_SIZE], fill=self.board_type)
+			for i in range(b_height-1):
+				drw.rectangle([0, SQ_SIZE*(i+1) - border, b_width*SQ_SIZE, SQ_SIZE*(i+1) + border], fill=BLACK)
+			for i in range(b_height-1):
+				drw.rectangle([SQ_SIZE*(i+1) - border, 0, SQ_SIZE*(i+1) + border, b_height*SQ_SIZE], fill=BLACK)
 
-        # POCKET
-        if pocket_rows:
-            drw.rectangle([0, b_height*SQ_SIZE, b_width*SQ_SIZE, b_height*SQ_SIZE + spacer], fill=BLACK)
-            
-            white_pocket = [white_hand[i:i + b_width] for i in range(0, len(white_hand), b_width)]
-            for i in range(len(white_pocket)):
-                for j in range(len(white_pocket[i])):
-                    piece = self.get_piece_img(white_pocket[i][j])
-                    img.paste(piece, (j*SQ_SIZE, (i+b_height)*SQ_SIZE + spacer), piece)
+		else: # custom image
+			board = PIL.Image.open(f'assets\\{self.folder}\\board.png')
+			if self.upside_down:
+				board = board.rotate(180)
+			img.paste(board, (0, 0))
 
-            black_pocket = [black_hand[i:i + b_width] for i in range(0, len(black_hand), b_width)]
-            for i in range(len(black_pocket)):
-                for j in range(len(black_pocket[i])):
-                    piece = self.get_piece_img(black_pocket[i][j])
-                    img.paste(piece, (j*SQ_SIZE, (i+b_height+len(white_pocket))*SQ_SIZE + spacer), piece)
+		# HIGHLIGHTING
+		if self.lastmove:
+			for highlight in findall(r'[a-z]\d+', self.lastmove):
+				coords = self.square_to_coords(highlight, b_width, b_height)
+				highlight_coords = coords + [i+100 for i in coords]
+				if self.board_type == "checkerboard":
+					if sum(coords) % (SQ_SIZE*2) == 0:
+						drw.rectangle(highlight_coords, fill=LASTMOVE_LIGHT)
+					else:
+						drw.rectangle(highlight_coords, fill=LASTMOVE_DARK)
+				else:
+					drw.rectangle(highlight_coords, fill=GREEN)
 
-        img.save(img_name)
+		# LETTERS
+		font_size = SQ_SIZE*0.2
+		font = PIL.ImageFont.truetype(FONT_LOCATION, round(font_size))
+
+		ranks = [str(n+1) for n in range(b_height)]
+		if not self.upside_down:
+			ranks = ranks[::-1]
+
+		files = [ALPHABET[n] for n in range(b_width)]
+		if self.upside_down:
+			files = files[::-1]
+
+		for i in range(b_height):
+			drw.text((b_width*SQ_SIZE - round(font_size*0.1), i*SQ_SIZE + round(font_size*0.2)), ranks[i], fill=(0, 0, 0), font=font, anchor="rt")
+
+		for i in range(b_width):
+			drw.text((i*SQ_SIZE + round(font_size*0.1), b_height*SQ_SIZE - round(font_size*0.1)), files[i], fill=(0, 0, 0), font=font, anchor="ls")
+
+		# PIECES
+		for i in range(len(pos)):
+			for j in range(len(pos[i])):
+				if pos[i][j]:
+					piece = self.get_piece_img(pos[i][j])
+					if self.flip_pieces:
+						piece = piece.rotate(180)
+					img.paste(piece, (j*SQ_SIZE, i*SQ_SIZE), piece)
+
+		# POCKET
+		if pocket_rows:
+			drw.rectangle([0, b_height*SQ_SIZE, b_width*SQ_SIZE, b_height*SQ_SIZE + spacer], fill=BLACK)
+
+			white_pocket = [white_hand[i:i + b_width] for i in range(0, len(white_hand), b_width)]
+			for i in range(len(white_pocket)):
+				for j in range(len(white_pocket[i])):
+					piece = self.get_piece_img(white_pocket[i][j])
+					img.paste(piece, (j*SQ_SIZE, (i+b_height)*SQ_SIZE + spacer), piece)
+
+			black_pocket = [black_hand[i:i + b_width] for i in range(0, len(black_hand), b_width)]
+			for i in range(len(black_pocket)):
+				for j in range(len(black_pocket[i])):
+					piece = self.get_piece_img(black_pocket[i][j])
+					img.paste(piece, (j*SQ_SIZE, (i+b_height+len(white_pocket))*SQ_SIZE + spacer), piece)
+
+		return img
+
+	def render_board(self, img_name):
+		self.draw_board().save(img_name)
+
+	@staticmethod
+	def scale_to_fit(width, height, img):
+		# resize the board the maximum amount
+		prev_width, prev_height = img.size
+		resize_factor = min(width / prev_width, height / prev_height)
+		new_width, new_height = int(prev_width * resize_factor), int(prev_height * resize_factor)
+		
+		scaled_img = img.resize((new_width, new_height), PIL.Image.ANTIALIAS)
+		
+		# create a black background
+		resized = PIL.Image.new(mode='RGB', size=(width, height), color=BLACK)
+		
+		# paste the board in the center of the black background
+		offset = ((width - new_width) // 2, (height - new_height) // 2)
+		resized.paste(scaled_img, offset)
+		
+		return resized
